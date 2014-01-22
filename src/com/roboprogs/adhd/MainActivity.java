2003692a362e0eeb671e5acaabcba9d0faed49a3
default_git_name <default_git_email>
2014-01-22 21:31:40 +0000

    WIP - try to wire up slider callback (get-by-ID is failing)

--- src/com/roboprogs/adhd/MainActivity.java
+++ src/com/roboprogs/adhd/MainActivity.java
@@ -7,7 +7,9 @@ package com.roboprogs.adhd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.library.*;
@@ -24,6 +26,14 @@ class					MainActivity
 	private
 	int					btnClicks = 0;
 
+	/** handle to slider progress bar */
+	private
+	SeekBar				seeker;
+
+	/** track slider position */
+	private
+	int					sliderPos = 0;
+
     /** Called when the activity is first created. */
     @Override
     public
@@ -33,6 +43,43 @@ class					MainActivity
 		)
 		{
         super.onCreate( icicle);
+
+		SeekBar.OnSeekBarChangeListener
+						seekHandler;
+
+		info( "About to create something");
+
+		this.seeker = (SeekBar) findViewById( R.id.busy_seek);
+
+		seekHandler = new SeekBar.OnSeekBarChangeListener()
+			{
+
+			/**
+			 * Track progress bar slider updates.
+			 */
+			public
+			void		onProgressChanged
+				(
+				SeekBar bar,
+				int		progress,
+				boolean fromUser
+				)
+				{
+/*
+				MainActivity.this.sliderPos = progress;
+				updateStatus();
+*/
+				}  // _____________________________________
+
+			public
+			void		onStartTrackingTouch( SeekBar bar)
+				{}
+			public
+			void		onStopTrackingTouch( SeekBar bar)
+				{}
+			} ;  // =======================================
+
+		// this.seeker.setOnSeekBarChangeListener( seekHandler);
 
         setContentView( R.layout.main);
 		}  // _____________________________________________
@@ -47,11 +94,34 @@ class					MainActivity
 		View			unused
 		)
 		{
+		this.btnClicks++;
+		updateStatus();
+		}  // _____________________________________________
+
+	/**
+	 * Update status text at bottomm of Busy-Box.
+	 */
+	public
+	void				updateStatus()
+		{
 		TextView		text;
 
-		this.btnClicks++;
 		text = (TextView) findViewById( R.id.busy_text);
-		text.setText( "Click count: " + this.btnClicks);
+		text.setText( "Click count: " + this.btnClicks +
+				", Pos " + this.sliderPos +
+				", Seeker = " + this.seeker);
+		}  // _____________________________________________
+
+	/**
+	 * Spout a log mmessage.
+	 */
+	public
+	void				info
+		(
+		String			msg
+		)
+		{
+		Log.i( MainActivity.class.getName(), msg);
 		}  // _____________________________________________
 
 	}  // =================================================
